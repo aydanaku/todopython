9e8957a165f3261e76a719397fcc301a6a3e343f
aydanaku <kochekbaevaaydana@gmail.com>
2021-01-21 22:36:09 +0600

    added todo marked

--- main/views.py
+++ main/views.py
@@ -27,6 +27,12 @@ def delete_todo(request, id):
     todo.delete()
     return redirect(test)
 
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
 def third(request):
     return HttpResponse("This is page test3.")
 
--- todo/urls.py
+++ todo/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("test2/", second),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
     path("test3/", third, name="homework"),
     path("bookshop/", book, name="book"),
     path("add-book/", add_book, name="add-book")
